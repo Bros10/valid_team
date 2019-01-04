866d4586106df13a41f483b7081ea7e8cf916e64
Bros10 <bossingbros10@gmail.com>
2019-01-04 11:35:43 +0000

    Add files via upload

---
+++ valid_team_id.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import urllib.request
+id_number = 1000502
+def loop(id_number):
+    while True:
+        if (len(str(id_number)) == 9):
+            break;
+        url = "https://play.eslgaming.com/team/matches/" + str(id_number)
+        id = url[len(url) - 8:len(url)]
+        link = urllib.request.urlopen(url).read()
+        game = 0
+        soup = BeautifulSoup(link, 'html.parser')
+        if "matches" in url:
+            error = (soup.find_all("strong"))
+            text = (soup.find_all("td"))
+        else:
+            text = (soup.find_all("b"))
+            error = (soup.find_all("strong"))
+        error = str(error)
+        clean_error = error
+        clean_error = "".join(clean_error.split())
+        clean_error_len = len(clean_error)
+        for counter in range(clean_error_len):
+            if error[counter:counter + 6] == "Error:":
+                id_number = id_number + 1
+                loop(id_number)
+        text = str(text)
+        print (id)
+        clean = text
+        clean = "".join(clean.split())
+        clean_len = len(clean)
+        for counter in range(clean_len):
+            if clean[counter:counter + 17] == "bgcolor=" + '"#7BA37F"':
+                game = game + 1
+            elif clean[counter:counter + 17] == "bgcolor=" + '"#EEE295"':
+                game = game + 1
+            elif clean[counter:counter + 17] == "bgcolor=" + '"#AC6060"':
+                game = game + 1
+        if (game >= 1):
+            with open("valid_teams.txt", 'a') as out:
+                out.write("\n" + str(id))
+        id_number = id_number + 1
+loop(id_number)
